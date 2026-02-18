5e960892e71bc0fd64ae035f261c2a7030d7ed3d
Eli Parish <z2003844@students.niu.edu>
2026-02-18 17:19:50 -0600

    Implement word_counter function

--- word_counter.js
+++ word_counter.js
@@ -4,3 +4,27 @@
 
 
 
+
+
+
+
+
+
+function word_counter(istring)
+{
+    
+   // const arr = istring.split(/\\S+/);
+    const arr = istring.trim().split(/\s+/)
+
+    //arr.length is the number of words in the string.
+
+
+    
+
+
+    console.log(arr);
+
+    return arr.length;
+}
+
+
